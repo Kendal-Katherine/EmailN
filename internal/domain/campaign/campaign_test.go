860f8ca717b8372bed5819f28c892eb6bba9ced4
Kendal Katherine <kendal.katherine3@gmail.com>
2024-04-07 13:35:09 -0300

    teste unitário v2 - Usando o Testify

--- internal/domain/campaign/campaign_test.go
+++ internal/domain/campaign/campaign_test.go
@@ -1,15 +1,20 @@
 package campaign
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestNewCampaign(t *testing.T) {
-
+	assert := assert.New(t)
 	name := "Campaign X"
 	content := "body"
 	contacts := []string{"email1@e.com", "email2@e.com"}
 
 	campaign := NewCampaign(name, content, contacts)
 
+	/* SUBSTITUIMOS ESSE MONTE DE IF/ELSE por assert
 	if campaign.ID != "1" {
 		t.Errorf("expected 1")
 	} else if campaign.Name != name {
@@ -18,5 +23,10 @@ func TestNewCampaign(t *testing.T) {
 		t.Errorf("expected correct content")
 	} else if len(campaign.Contacts) != len(contacts) {
 		t.Errorf("expected correct contacts")
-	}
+	}*/
+	
+	assert.Equal(campaign.ID, "1")
+	assert.Equal(campaign.Name, name)
+	assert.Equal(campaign.Content, content)
+	assert.Equal(len(campaign.Contacts), len(contacts))
 }
