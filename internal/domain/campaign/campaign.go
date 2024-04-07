96a5c565270fa2b7dc22944c101307c5f4d373da
Kendal Katherine <kendal.katherine3@gmail.com>
2024-04-07 13:21:47 -0300

    teste de unidade v1

--- internal/domain/campaign/campaign.go
+++ internal/domain/campaign/campaign.go
@@ -2,7 +2,7 @@ package campaign
 
 import "time"
 
-type Contacts struct{
+type Contacts struct {
 	Email string
 }
 
@@ -10,22 +10,22 @@ type Campaign struct {
 	ID        string
 	Name      string
 	CreatedOn time.Time
-	Content string
-	Contacts []Contacts
+	Content   string
+	Contacts  []Contacts
 }
 
-func NewCampaign(name string, content string, emails []string) Campaign{
-	
+func NewCampaign(name string, content string, emails []string) *Campaign {
+
 	contacts := make([]Contacts, len(emails))
-	for index, email := range emails{
+	for index, email := range emails {
 		contacts[index].Email = email
 	}
-	
-	return Campaign{
-		ID: "1",
-		Name: name,
-		Content:  content,
+
+	return &Campaign{
+		ID:        "1",
+		Name:      name,
+		Content:   content,
 		CreatedOn: time.Now(),
 		Contacts:  contacts,
-		}
 	}
+}
