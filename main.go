23e2aa7e18e65715ae4841805925a2d8688b6c57
Kendal Katherine <kendal.katherine3@gmail.com>
2024-04-07 18:13:04 -0300

    validando struct

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"emailn/internal/domain/campaign"
+
+	"github.com/go-playground/validator/v10"
+)
+
+func main() {
+	
+	campaign := campaign.Campaign{}
+	validate := validator.New()
+	err := validate.Struct(campaign)
+	if err == nil {
+		println("Nenhum erro")
+	} else {
+		validationErrors := err.(validator.ValidationErrors)
+		for _, v := range validationErrors {
+
+			switch v.Tag() {
+			case "required":
+				println(v.StructField() + " is required")
+			case "min":
+				println(v.StructField() + " is required with min " + v.Param())
+			case "max":
+				println(v.StructField() + " is required with max " + v.Param())
+			case "email":
+				println(v.StructField() + " is invalid ")
+			}
+
+			println(v.StructField() + " is invalid: " + v.Tag())
+		}
+	}
+}
